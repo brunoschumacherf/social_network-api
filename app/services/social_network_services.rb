891dc56b288c1ad7744a69d30bda7a996a540446
Bruno Schumacher <brunoschumacherfsouza@gmail.com>
2023-08-12 16:38:48 -0300

    fix(SN-2): adding service for use of dry

---
+++ app/services/social_network_services.rb
@@ -0,0 +1,55 @@
+class SocialNetworkService
+  require 'dry/monads/result'
+  require 'dry/matcher/result_matcher'
+  include Dry::Monads[:result, :do]
+
+  def self.call(*args, &block)
+    result = new(*args, &block).call
+
+    Rails.logger.info "\n
+                        ############### Failure Service #{self} ###############\n
+                        step: #{result.failure.first}\n
+                        reason: #{result.failure.last}\n"
+
+    result.failure << {} if result.failure?
+
+    if result.failure?
+      result.failure << { errors: { step: result.failure.first, failure: result.failure.last } }
+    end
+
+    if block_given?
+      Dry::Matcher::ResultMatcher.call(result, &block)
+    else
+      result
+    end
+  end
+
+  private
+
+  def parameters(&block)
+    @parameters = block
+  end
+
+  def rules(&block)
+    @rules = block
+  end
+
+  def validator
+    local_parameters = @parameters
+    local_rules      = @rules
+
+    @validator ||= Class.new(::BaseContract) do
+      params do
+        instance_exec(&local_parameters)
+      end
+      instance_exec(&local_rules) if local_rules
+    end.new
+  end
+
+  def validate(data)
+    result = validator.call(data.to_h)
+    return Failure[:validate_params, result.errors.to_h] if result.failure?
+
+    Success(result.to_h)
+  end
+end
